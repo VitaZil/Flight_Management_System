b3cd7721a08135a1c9e7cabc235cc77c921ea677
Vita Žilionė <102750381+VitaZil@users.noreply.github.com>
2023-01-09 12:23:46 +0200

    add no flights

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -4,7 +4,6 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Flight Management System</title>
-    <script src="//unpkg.com/alpinejs" defer></script>
     <script src="https://cdn.tailwindcss.com"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
--- resources/views/flights/index.blade.php
+++ resources/views/flights/index.blade.php
@@ -1,55 +1,61 @@
 <x-layout>
     <div class="filter-container">
+
         <div class="filter-box">
-
-    <div class="filter">
-        <form action="/flights/filter" class="flight-form">
-            @csrf
-            <div class="input-container form-converter input-filter">
-                <select class="input" name="newtimezone" id="newtimezone" required>
-                    @foreach($timezones as $timezone)
-                        <option value="{{ $timezone }}">{{ $timezone }}</option>
-                    @endforeach
-                </select>
+            <div class="filter">
+                <form action="/flights/filter" class="flight-form">
+                    @csrf
+                    <div class="input-container form-converter input-filter">
+                        <select class="input" name="newtimezone" id="newtimezone" required>
+                            @foreach($timezones as $timezone)
+                                <option value="{{ $timezone }}">{{ $timezone }}</option>
+                            @endforeach
+                        </select>
+                    </div>
+                    <button class="button-flights">Change Time Zone</button>
+                </form>
             </div>
-            <button class="button-flights">Change Time Zone</button>
-        </form>
-    </div>
-    </div>
+        </div>
+
         <h1 class="title-table">Flights</h1>
+        @if(count($flights) == 0)
+            <p class="no-flights">There is no flights</p>
+        @else
 
-    <table class="">
-        <tr>
-            <th>Details</th>
-            <th>Departure Time</th>
-            <th>Departure Airport</th>
-            <th>Arrival Time</th>
-            <th>Arrival Airport</th>
-            <th>Time Zone</th>
-            <th>Number Of Seats</th>
-            <th>Action</th>
-        </tr>
-        @foreach($flights as $flight)
-            <tr>
-                <td><a href="flights/{{$flight->id}}" class="button-flights">View</a></td>
-                <td>{{ $flight->departure_time }}</td>
-                <td>{{ $flight->departureAirport->name }}</td>
-                <td>{{ $flight->arrival_time }}</td>
-                <td>{{ $flight->arrivalAirport->name }}</td>
-                <td>{{ $flight->timezone }}</td>
-                <td>{{ $flight->seats }}</td>
-                <td>
+            <table>
+                <tr>
+                    <th>Details</th>
+                    <th>Departure Time</th>
+                    <th>Departure Airport</th>
+                    <th>Arrival Time</th>
+                    <th>Arrival Airport</th>
+                    <th>Time Zone</th>
+                    <th>Number Of Seats</th>
+                    <th>Action</th>
+                </tr>
+                @foreach($flights as $flight)
+                    <tr>
+                        <td><a href="flights/{{$flight->id}}" class="button-flights">View</a></td>
+                        <td>{{ $flight->departure_time }}</td>
+                        <td>{{ $flight->departureAirport->name }}</td>
+                        <td>{{ $flight->arrival_time }}</td>
+                        <td>{{ $flight->arrivalAirport->name }}</td>
+                        <td>{{ $flight->timezone }}</td>
+                        <td>{{ $flight->seats }}</td>
+                        <td>
 
-                    <a class="button-flights" href="/flights/{{ $flight->id }}/edit">Edit</a>
-                    <form method="POST" action="/flights/{{ $flight->id }}/delete">
-                        @csrf
-                        @method('DELETE')
-                        <input type="hidden" value="{{ $flight->id }}">
-                        <button class="button-flights" type="submit">Delete</button>
-                    </form>
-                </td>
-            </tr>
-        @endforeach
-    </table>
-</div>
+                            <a class="button-flights" href="/flights/{{ $flight->id }}/edit">Edit</a>
+                            <form method="POST" action="/flights/{{ $flight->id }}/delete">
+                                @csrf
+                                @method('DELETE')
+                                <input type="hidden" value="{{ $flight->id }}">
+                                <button class="button-flights" type="submit">Delete</button>
+                            </form>
+                        </td>
+                    </tr>
+                @endforeach
+            </table>
+        @endif
+    </div>
+
 </x-layout>
